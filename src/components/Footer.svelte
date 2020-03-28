ca43cb93f9a26e493a8717ac38683879aab8cdf6
Josh Weaver <jweaver@ministrysync.com>
2020-03-28 18:18:10 -0400

    updates footer component

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,12 @@
+<script>
+export let extraFooter
+
+</script>
 <footer class="footer">
 	<div class="content has-text-centered has-text-dark">
 		<p>
 			Copyright &copy; 2020 Josh Weaver All Rights Reserved
 		</p>
+		<slot></slot>
 	</div>
 </footer>
